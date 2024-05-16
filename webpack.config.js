c689cb4a43f73cca8c268231e260d30b8ee72319
CO <99570414+codeFromCO@users.noreply.github.com>
2024-05-16 17:54:25 -0400

    check webpack proxy

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,11 @@ module.exports = {
     port: 8080,
     hot: true,
     historyApiFallback: true,
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:3000',
+      },
+    ],
   },
 };
