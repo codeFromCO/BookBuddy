6d66e9ceeb7fb876c650bba268793b2de4f3afc7
CO <99570414+codeFromCO@users.noreply.github.com>
2024-05-16 19:32:55 -0400

    add skeleton for components search, book cover and book card

---
+++ src/components/BarSearch.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const BarSearch = (props) => {};
+
+export default BarSearch;
---
+++ src/components/BookCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookCard = (props) => {
+
+}
+
+export default BookCard
