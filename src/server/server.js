7fe7410da4f4773420d5d3f9536ac4b1137e9ead
CO <99570414+codeFromCO@users.noreply.github.com>
2024-05-16 17:08:26 -0400

    set up initial server structure

---
+++ src/server/server.js
@@ -0,0 +1,36 @@
+// import modules
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+// import modules relating to db
+
+// import controllers
+
+// import model
+
+// create instance of express app
+const app = express();
+
+// parse incoming requests
+app.use(express.json());
+
+// allows for parsing of requests with Content-Type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// serve bundle
+app.use(express.static(path.join(__dirname, '../../dist')));
+
+// establish db connection
+
+// catch all 
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
+  });
+  
+// initialise server
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on PORT ${process.env.PORT}`);
+});
+
+module.exports = app;
