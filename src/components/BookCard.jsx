2486ac097cd5f0c5dde485f68a5615d48c33d08d
CO <99570414+codeFromCO@users.noreply.github.com>
2024-05-17 11:36:26 -0400

    add edit icon to book card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FaRegEdit } from 'react-icons/fa';
 
 const BookCard = (props) => {
   return (
     <div className='bg-red-500 p-3 rounded-md'>
-      <h2 className='font-bold text-lg'>{props.title}</h2>
+      <div className='flex justify-between'>
+        <h2 className='font-bold text-lg'>{props.title}</h2>
+        <FaRegEdit
+          className='size-5 hover:cursor-pointer'
+          onClick={props.onClick}
+        />
+      </div>
       <h3 className='text-sm mb-2'>{props.author}</h3>
       <p className='text-sm'>{props.notes}</p>
     </div>
