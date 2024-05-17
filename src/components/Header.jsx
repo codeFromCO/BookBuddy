798eda28203766b3085282609edc5fde25178aa2
CO <99570414+codeFromCO@users.noreply.github.com>
2024-05-17 11:30:38 -0400

    add icon to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { FaBookReader } from "react-icons/fa";
+
 const Header = (props) => {
   return (
-    <header>
-      <h1>{props.title}</h1>
+    <header className='flex items-center gap-x-2 bg-yellow-500'>
+      <h1 className='text-4xl'>{props.title}</h1>
+      <FaBookReader className='size-8 text-red-400' />
     </header>
   );
 };
 
-export default Header
+export default Header;
