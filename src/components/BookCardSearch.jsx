40ec5a5fee33494f5e9834056dee11e9a28de6f2
CO <99570414+codeFromCO@users.noreply.github.com>
2024-06-03 17:07:40 -0400

    for search card push + to far right

--- src/components/BookCardSearch.jsx
+++ src/components/BookCardSearch.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { IoAddOutline } from "react-icons/io5";
+import { IoAddOutline } from 'react-icons/io5';
 
 const BookCardSearch = (props) => {
   return (
     <div className='bg-gray-100 p-1 w-inputSearchWidth flex'>
-      <div className='flex'>
-        <img className='h-20' src={props.src}></img>
-        <div className='flex-column justify-between pl-3'>
+      <img className='h-20' src={props.src}></img>
+      <div className='flex-column pl-3 w-full'>
+        <div className='flex justify-between'>
           <h2 className='font-bold text-sm'>{props.title}</h2>
-          <h3 className='text-sm mb-2'>{props.author}</h3>
+          <IoAddOutline
+            className='size-5 hover:cursor-pointer'
+            onClick={props.onClick}
+          />
         </div>
-        <IoAddOutline className='size-5 hover:cursor-pointer' onClick={props.onClick}/>
+        <h3 className='text-sm mb-2'>{props.author}</h3>
       </div>
     </div>
   );
