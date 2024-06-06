928b3bea10a3a3f84cb99885d756734ba26f58f1
CO <99570414+codeFromCO@users.noreply.github.com>
2024-06-06 14:44:06 -0400

    consolidate home page and test home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import HomePage from './pages/home';
-import TestHome from './pages/homeNotes';
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
-        <Route path='/testhome' element={<TestHome/>}></Route>
       </Routes>
     </div>
   );
