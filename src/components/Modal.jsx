65ecdf49ad01d5705884bf8800813814a09587a2
CO <99570414+codeFromCO@users.noreply.github.com>
2024-06-08 18:29:23 -0400

    fix responsive issue with modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,19 +3,12 @@ import Button from './Button';
 
 const Modal = (props) => {
   return (
-    <div className='fixed h-full w-full z-10 ml-[-20px]'>
-      {' '}
-      {/* hacky solution to gutter issue */}
-      <div className='h-full w-full '></div>
-      <div className='flex flex-col px-5 pt-7 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-[500px] w-11/12 h-[170px] bg-white rounded-lg text-baseTextPrimary border-2 border-baseTextPrimary '>
-        <div className='text-xl font-medium'>{props.heading}</div>
-        <div className='text-sm mt-2'>{props.subheading}</div>
-        <div className='flex-grow'></div>{' '}
-        {/* This will make the space between content and buttons flexible */}
-        <div className='flex justify-end pb-5 gap-x-2'>
-          <Button tertiary='true' name='Confirm' onClick={props.okay} />
-          <Button name='Cancel' onClick={props.cancel} />
-        </div>
+    <div className='flex flex-col px-5 pt-7 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-[500px] w-11/12 bg-white rounded-lg text-baseTextPrimary border-2 border-baseTextPrimary '>
+      <div className='text-xl font-medium'>{props.heading}</div>
+      <div className='text-sm my-2'>{props.subheading}</div>
+      <div className='flex justify-end pb-5 gap-x-2'>
+        <Button tertiary='true' name='Confirm' onClick={props.okay} />
+        <Button name='Cancel' onClick={props.cancel} />
       </div>
     </div>
   );
