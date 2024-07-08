aca396f15c4694fded1a692aec8d1b47f154e252
CO <99570414+codeFromCO@users.noreply.github.com>
2024-07-08 14:34:16 -0400

    remove constants to own file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 export const bookSearchAPI = 'https://openlibrary.org/search.json?q=';
 
-
 export const bookcoverAPI = 'https://covers.openlibrary.org/b/id/';
 
 export const API_URL = '/api/book';
