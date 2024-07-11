c58c26e23a29b5a34b3d65b169b557d0add592e6
CO <99570414+codeFromCO@users.noreply.github.com>
2024-07-11 15:29:00 -0400

    change test case for normalizeString

--- __TESTS__/Functions.test.js
+++ __TESTS__/Functions.test.js
@@ -1,6 +1,5 @@
 import { normalizeString } from '../src/utils/functions';
 
 test('normalize string return a lowercase string and trims white spaces', () => {
-  expect(normalizeString('THIS IS A TEST')).toBe('this is a test');
-  expect(normalizeString(' This is a test ')).toBe('this is a test');
+  expect(normalizeString(' THIS IS A TEST ')).toBe('this is a test');
 });
