da23f976613422fffdd921c096a664e636d616ee
CO <99570414+codeFromCO@users.noreply.github.com>
2024-07-11 14:43:36 -0400

    fix issue with getBy vs findBy and declare mocks

--- __TESTS__/Home.test.js
+++ __TESTS__/Home.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
 import HomePage from '../src/pages/home';
 
 // mock API function
@@ -13,9 +12,11 @@ jest.mock('../src/api/api', () => ({
 // mock components
 jest.mock('../src/components/Header', () => () => <div>Header</div>);
 jest.mock('../src/components/SideBar', () => () => <div>SideBar</div>);
-jest.mock('../src/components/Error', () => ({ alert }) => <div>{alert}</div>);
+jest.mock('../src/components/ButtonLoading', () => () => (
+  <div data-testid='button-loading'>ButtonLoading</div>
+));
 jest.mock('../src/components/CardBook', () => ({ title, author, onClick }) => (
-  <div onClick={onClick}>
+  <div data-testid='card-book' onClick={onClick}>
     {title} - {author}
   </div>
 ));
@@ -28,25 +29,65 @@ jest.mock('../src/components/ModalBook', () => ({ title, author }) => (
     <div>Author: {author}</div>
   </div>
 ));
-jest.mock('../src/components/ModalAlert', () => (
-  <div data-testid='modal-alert'>ModalAlert</div>
-));
 
-const queryClient = new QueryClient();
+
+// mock data
+const mockBooks = [
+  {
+    title: 'Book 1',
+    author: 'Author 1',
+    cover_i: 1,
+    _id: '1',
+    notes: 'Notes 1',
+  },
+  {
+    title: 'Book 2',
+    author: 'Author 2',
+    cover_i: 2,
+    _id: '2',
+    notes: 'Notes 2',
+  },
+  {
+    title: 'Book 3',
+    author: 'Author 3',
+    cover_i: 3,
+    _id: '3',
+    notes: 'Notes 3',
+  },
+];
+
+const emptyMockBooks = [];
+
+const cleanClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 const renderHomePage = () =>
   render(
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <HomePage />
-      </BrowserRouter>
+    <QueryClientProvider client={cleanClient}>
+      <HomePage />
     </QueryClientProvider>
   );
 
 describe('Home Page', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    cleanup();
+  });
+
+  afterEach(() => {
+    cleanClient.clear();
+  });
+
   test('renders the components on the home page', () => {
+    // render HomePage component
     renderHomePage();
 
+    // assert that the modal displays the header, sidebar, search input and '+ Add new book' button
     expect(screen.getByText(/Header/i)).toBeInTheDocument();
     expect(screen.getByText(/SideBar/i)).toBeInTheDocument();
     expect(
@@ -55,91 +96,70 @@ describe('Home Page', () => {
     expect(screen.getByText(/\+ Add new book/i)).toBeInTheDocument();
   });
 
-  test('renders error message if there are no existing books', () => {
-    const mockEmptyBooks = [];
+  test('renders error message if there are no existing books', async () => {
+    // mock fetckBooks API function
+    require('../src/api/api').fetchBooks.mockResolvedValueOnce(emptyMockBooks);
 
-    require('../src/api/api').fetchBooks.mockResolvedValueOnce(mockEmptyBooks);
-
+    // render HomePage component
     renderHomePage();
 
-    expect(screen.getByText(/No books found/i)).toBeInTheDocument();
+    // assert that the modal displays an error message
+    expect(await screen.findByText(/No books found/i)).toBeInTheDocument();
+
+    // assert that no book cards are in the document
+    expect(screen.queryByTestId('card-book')).not.toBeInTheDocument();
   });
 
   test('renders existing books', async () => {
-    const mockBooks = [
-      {
-        title: 'Book 1',
-        author: 'Author 1',
-        cover_i: 1,
-        _id: '1',
-        notes: 'Notes 1',
-      },
-      {
-        title: 'Book 2',
-        author: 'Author 2',
-        cover_i: 2,
-        _id: '2',
-        notes: 'Notes 2',
-      },
-    ];
-
+    // mock fetckBooks API function
     require('../src/api/api').fetchBooks.mockResolvedValueOnce(mockBooks);
 
+    // render HomePage component
     renderHomePage();
 
-    await screen.findByText('Book 1 - Author 1');
-    await screen.findByText('Book 2 - Author 2');
-
-    // check books are rendered
-    expect(screen.getByText(/Book 1 - Author 1/i)).toBeInTheDocument();
-    expect(screen.getByText(/Book 2 - Author 2/i)).toBeInTheDocument();
+    // assert that there were be a corresponding CardBook for every book
+    expect(await screen.findByText(/Book 1 - Author 1/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Book 2 - Author 2/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Book 3 - Author 3/i)).toBeInTheDocument();
   });
 
-  test('opens search modal upon clicking + Add new book button', () => {
+  test(`opens search modal upon clicking '+ Add new book' button`, async () => {
+    // render HomePage component
     renderHomePage();
 
+    // simulate clicking '+ Add new book' button
     fireEvent.click(screen.getByText(/\+ Add new book/i));
 
-    expect(screen.getByTestId('modal-search')).toBeInTheDocument();
+    // assert that ModalSearch component will be displayed
+    expect(await screen.findByTestId('modal-search')).toBeInTheDocument();
   });
 
   test('opens book modal and display existing book data when an existing book is clicked', async () => {
-    const mockBook = [
-      {
-        title: 'Book 1',
-        author: 'Author 1',
-        cover_i: 1,
-        _id: '1',
-        notes: 'Notes 1',
-      },
-    ];
+    // mock fetckBooks API function
+    require('../src/api/api').fetchBooks.mockResolvedValueOnce(mockBooks);
 
-    require('../src/api/api').fetchBooks.mockResolvedValueOnce(mockBook);
-
+    // render HomePage component
     renderHomePage();
 
-    fireEvent.click(screen.getByText(/Book 1 - Author 1/i));
+    // wait for the book to appear and simulate a click
+    fireEvent.click(await screen.findByText(/Book 1 - Author 1/i));
 
-    expect(screen.getByTestId('modal-book')).toBeInTheDocument();
+    // wait for the modal to appear in the document
+    expect(await screen.findByTestId('modal-book')).toBeInTheDocument();
+
+    // assert that the modal displays the correct details
     expect(screen.getByText('Title: Book 1')).toBeInTheDocument();
     expect(screen.getByText('Author: Author 1')).toBeInTheDocument();
   });
 
-  test(`hides existing books and displays a 'No books found' error message if the title search for an existing book yields no results`, () => {
-    const mockBook = [
-      {
-        title: 'Book 1',
-        author: 'Author 1',
-        cover_i: 1,
-        _id: '1',
-        notes: 'Notes 1',
-      },
-    ];
+  test(`hides existing books and displays a 'No books found' error message if the title search for an existing book yields no results`, async () => {
+    // mock fetckBooks API function
+    require('../src/api/api').fetchBooks.mockResolvedValueOnce(mockBooks);
 
-    require('../src/api/api').fetchBooks.mockResolvedValueOnce(mockBook);
-
+    // render HomePage component
     renderHomePage();
 
+    // simulate typing search query into search input
     fireEvent.change(
       screen.getByPlaceholderText(/Search existing books by title/i),
       {
@@ -147,12 +167,46 @@ describe('Home Page', () => {
       }
     );
 
-    expect(screen.getByText(/No books found/i)).toBeInTheDocument();
+    // assert that an error will appear in the document
+    expect(await screen.findByText(/No books found/i)).toBeInTheDocument();
 
-    expect(screen.queryByText(/Book 1 - Author 1/i)).not.toBeInTheDocument();
+    // assert that no CardBooks will appear in the document
+    expect(screen.queryByTestId('card-book')).not.toBeInTheDocument();
+  });
+
+  test(`if a book is searched for and exists, only that book will be displayed`, async () => {
+    // mock fetckBooks API function
+    require('../src/api/api').fetchBooks.mockResolvedValueOnce(mockBooks);
+
+    // render HomePage component
+    renderHomePage();
+
+    // simulate typing search query into search input
+    fireEvent.change(
+      screen.getByPlaceholderText(/Search existing books by title/i),
+      {
+        target: { value: 'Book 1' },
+      }
+    );
+
+    // assert that the matching book will be displayed
+    expect(await screen.findByText(/Book 1 - Author 1/i)).toBeInTheDocument();
+
+    // assert that books not matching the query will not be displayed
+    expect(screen.queryByText(/Book 2 - Author 2/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/Book 3 - Author 3/)).not.toBeInTheDocument();
+  });
+
+  test('displays a ButtonLoading component while data is being fetched', async () => {
+    // simulate the fetching state with no data
+    require('../src/api/api').fetchBooks.mockImplementation(
+      () => new Promise(() => {})
+    );
+
+    // render HomePage component
+    renderHomePage();
+
+    // assert that a loading button will be displayed whilst books are being fetched
+    expect(screen.getByTestId('button-loading')).toBeInTheDocument();
   });
 });
-
-// display filterd book
-// display buttonLoading on fetch
-// display modal alert
