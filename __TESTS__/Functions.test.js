0cc5fc7bff20c430ea901bae8480de53927f570d
CO <99570414+codeFromCO@users.noreply.github.com>
2024-07-15 17:42:45 -0400

    add test cases for sorting functions

--- __TESTS__/Functions.test.js
+++ __TESTS__/Functions.test.js
@@ -1,5 +1,220 @@
-import { normalizeString } from '../src/utils/functions';
+import {
+  normalizeString,
+  sortAlphabetically,
+  sortAddedNewOld,
+  sortAddedOldNew,
+  sortUpdatedNewOld,
+  sortUpdatedOldNew,
+} from '../src/utils/functions';
 
-test('normalize string return a lowercase string and trims white spaces', () => {
+const mockBooks = [
+  {
+    id: 5,
+    title: 'Book 5',
+    added: new Date('2024-07-12'),
+    updated: new Date('2024-07-13'),
+  },
+  {
+    id: 3,
+    title: 'Book 3',
+    added: new Date('2024-07-14'),
+    updated: new Date('2024-07-20'),
+  },
+  {
+    id: 1,
+    title: 'Book 1',
+    added: new Date('2024-07-15'),
+    updated: new Date('2024-07-30'),
+  },
+  {
+    id: 4,
+    title: 'Book 4',
+    added: new Date('2024-07-16'),
+    updated: new Date('2024-07-17'),
+  },
+  {
+    id: 2,
+    title: 'Book 2',
+    added: new Date('2024-07-13'),
+    updated: new Date('2024-07-15'),
+  },
+];
+
+test('normalize string returns a lowercase string and trims white spaces', () => {
   expect(normalizeString(' THIS IS A TEST ')).toBe('this is a test');
+});
+
+test('sortAlphabetically sorts an array by title', () => {
+  expect(sortAlphabetically(mockBooks)).toEqual([
+    {
+      id: 1,
+      title: 'Book 1',
+      added: new Date('2024-07-15'),
+      updated: new Date('2024-07-30'),
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      added: new Date('2024-07-13'),
+      updated: new Date('2024-07-15'),
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      added: new Date('2024-07-14'),
+      updated: new Date('2024-07-20'),
+    },
+    {
+      id: 4,
+      title: 'Book 4',
+      added: new Date('2024-07-16'),
+      updated: new Date('2024-07-17'),
+    },
+    {
+      id: 5,
+      title: 'Book 5',
+      added: new Date('2024-07-12'),
+      updated: new Date('2024-07-13'),
+    },
+  ]);
+});
+
+test('sortAddedNewOld sorts an array from newest to oldest in terms of date added', () => {
+  expect(sortAddedNewOld(mockBooks)).toEqual([
+    {
+      id: 4,
+      title: 'Book 4',
+      added: new Date('2024-07-16'),
+      updated: new Date('2024-07-17'),
+    },
+    {
+      id: 1,
+      title: 'Book 1',
+      added: new Date('2024-07-15'),
+      updated: new Date('2024-07-30'),
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      added: new Date('2024-07-14'),
+      updated: new Date('2024-07-20'),
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      added: new Date('2024-07-13'),
+      updated: new Date('2024-07-15'),
+    },
+    {
+      id: 5,
+      title: 'Book 5',
+      added: new Date('2024-07-12'),
+      updated: new Date('2024-07-13'),
+    },
+  ]);
+});
+
+test('sortAddedOldNew sorts an array from oldest to newest in terms of date added', () => {
+  expect(sortAddedOldNew(mockBooks)).toEqual([
+    {
+      id: 5,
+      title: 'Book 5',
+      added: new Date('2024-07-12'),
+      updated: new Date('2024-07-13'),
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      added: new Date('2024-07-13'),
+      updated: new Date('2024-07-15'),
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      added: new Date('2024-07-14'),
+      updated: new Date('2024-07-20'),
+    },
+    {
+      id: 1,
+      title: 'Book 1',
+      added: new Date('2024-07-15'),
+      updated: new Date('2024-07-30'),
+    },
+    {
+      id: 4,
+      title: 'Book 4',
+      added: new Date('2024-07-16'),
+      updated: new Date('2024-07-17'),
+    },
+  ]);
+});
+
+test('sortUpdatedOldNew sorts an array from oldest to newest in terms of date updated', () => {
+  expect(sortUpdatedOldNew(mockBooks)).toEqual([
+    {
+      id: 5,
+      title: 'Book 5',
+      added: new Date('2024-07-12'),
+      updated: new Date('2024-07-13'),
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      added: new Date('2024-07-13'),
+      updated: new Date('2024-07-15'),
+    },
+    {
+      id: 4,
+      title: 'Book 4',
+      added: new Date('2024-07-16'),
+      updated: new Date('2024-07-17'),
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      added: new Date('2024-07-14'),
+      updated: new Date('2024-07-20'),
+    },
+    {
+      id: 1,
+      title: 'Book 1',
+      added: new Date('2024-07-15'),
+      updated: new Date('2024-07-30'),
+    },
+  ]);
+});
+
+test('sortUpdatedNewOld sorts an array from newest to oldest in terms of date updated', () => {
+  expect(sortUpdatedNewOld(mockBooks)).toEqual([
+    {
+      id: 1,
+      title: 'Book 1',
+      added: new Date('2024-07-15'),
+      updated: new Date('2024-07-30'),
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      added: new Date('2024-07-14'),
+      updated: new Date('2024-07-20'),
+    },
+    {
+      id: 4,
+      title: 'Book 4',
+      added: new Date('2024-07-16'),
+      updated: new Date('2024-07-17'),
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      added: new Date('2024-07-13'),
+      updated: new Date('2024-07-15'),
+    },
+    {
+      id: 5,
+      title: 'Book 5',
+      added: new Date('2024-07-12'),
+      updated: new Date('2024-07-13'),
+    },
+  ]);
 });
