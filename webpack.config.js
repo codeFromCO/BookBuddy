12094dd867264eaf17ab9ca791f894819e5e74c0
CO <99570414+codeFromCO@users.noreply.github.com>
2024-07-31 13:12:26 -0400

    ensure dyanmic webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default {
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, './dist'),
