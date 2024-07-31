e6478848175a4af61b6280817dd56da743aa8123
CO <99570414+codeFromCO@users.noreply.github.com>
2024-07-31 12:38:12 -0400

    fix styling on empty state

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { FaWorm } from 'react-icons/fa6';
 
-const EmptyState = (props) => {
+const EmptyState = () => {
   return (
     <div
       className={`flex flex-col w-full h-full justify-center items-center mb-5`}
     >
-      <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-col justify-center items-center text-center'>
         <FaWorm className='size-20 mb-3' alt='Empty state icon' />
         <h2 className='text-xl font-semibold'>Nothing here, but me</h2>
-        <p>Click on the 'add book' button to get started.</p>
+        <p className='sm:flex hidden'>
+          Click on the 'add book' button to get started.
+        </p>
+        <p className='sm:hidden'>Click on the '+' button to get started.</p>
       </div>
     </div>
   );
