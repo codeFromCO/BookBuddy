29c0339df68454ab684896f5f9be815ffac8e13d
CO <99570414+codeFromCO@users.noreply.github.com>
2024-08-06 09:56:41 -0400

    change sidebar font to sm

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -12,72 +12,70 @@ const SideBar = (props) => {
       onMouseOver={() => setShowSidebarNames(true)}
       onMouseOut={() => setShowSidebarNames(false)}
     >
-      <div>
-        <div
-          className={`fixed flex flex-col bg-primary h-screen pt-16 gap-5 border-r-2 p-1 border-r-primaryAccent
+      <div
+        className={`fixed flex flex-col bg-primary h-screen pt-16 gap-5 border-r-2 p-1 border-r-primaryAccent
           invisible sm:visible z-20`}
+      >
+        <Link
+          to='/'
+          className={`${
+            props.active === 'home'
+              ? 'bg-primaryFocus'
+              : 'hover:bg-primaryFocus'
+          }  p-3 space-x-2 h-10 flex items-center rounded-md`}
+          aria-label='Home'
         >
-          <Link
-            to='/'
-            className={`${
-              props.active === 'home'
-                ? 'bg-primaryFocus'
-                : 'hover:bg-primaryFocus'
-            }  p-3 space-x-2 h-10 flex items-center rounded-md`}
-            aria-label='Home'
-          >
-            <GiOpenBook size='1.5em' />
-            {showSidebarNames && (
-              <div
-                className={`
+          <GiOpenBook size='1.5em' />
+          {showSidebarNames && (
+            <div
+              className={`text-sm 
           ${props.active === 'home' ? 'font-bold' : ''} `}
-              >
-                Home
-              </div>
-            )}
-          </Link>
-          <Link
-            to='/stats'
-            className={`${
-              props.active === 'stats'
-                ? 'bg-primaryFocus'
-                : 'hover:bg-primaryFocus'
-            }  p-3 space-x-2 h-10 flex items-center rounded-md`}
-            aria-label='Stats'
-          >
-            <IoStatsChartSharp size='1.5em' />
-            {showSidebarNames && (
-              <div
-                className={`
+            >
+              Home
+            </div>
+          )}
+        </Link>
+        <Link
+          to='/stats'
+          className={`${
+            props.active === 'stats'
+              ? 'bg-primaryFocus'
+              : 'hover:bg-primaryFocus'
+          }  p-3 space-x-2 h-10 flex items-center rounded-md`}
+          aria-label='Stats'
+        >
+          <IoStatsChartSharp size='1.5em' />
+          {showSidebarNames && (
+            <div
+              className={`text-sm 
           ${props.active === 'stats' ? 'font-bold' : ''} `}
-              >
-                Stats
-              </div>
-            )}
-          </Link>
-          <Link
-            to='/settings'
-            className={`${
-              props.active === 'settings'
-                ? 'bg-primaryFocus'
-                : 'hover:bg-primaryFocus'
-            }  p-3 space-x-2 h-10 flex items-center justify-center rounded-md`}
-            aria-label='Settings'
-          >
-            <IoSettingsSharp size='1.5em' />
-            {showSidebarNames && (
-              <div
-                className={`
+            >
+              Stats
+            </div>
+          )}
+        </Link>
+        <Link
+          to='/settings'
+          className={`${
+            props.active === 'settings'
+              ? 'bg-primaryFocus'
+              : 'hover:bg-primaryFocus'
+          }  p-3 space-x-2 h-10 flex items-center justify-center rounded-md`}
+          aria-label='Settings'
+        >
+          <IoSettingsSharp size='1.5em' />
+          {showSidebarNames && (
+            <div
+              className={`text-sm 
           ${props.active === 'settings' ? 'font-bold' : ''} `}
-              >
-                Settings
-              </div>
-            )}
-          </Link>
-        </div>
+            >
+              Settings
+            </div>
+          )}
+        </Link>
       </div>
     </div>
   );
-}
+};
 
-export default SideBar 
+export default SideBar;
