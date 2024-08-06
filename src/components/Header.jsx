162202f389dd4b79a8ec0997fdf8d44500b1bea9
CO <99570414+codeFromCO@users.noreply.github.com>
2024-08-06 10:17:45 -0400

    fix header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,23 @@ import { IoMdAdd } from 'react-icons/io';
 
 const Header = (props) => {
   return (
-    <header className='flex justify-between items-center mt-3'>
+    <header className='flex justify-between items-start mt-3'>
       <button
         className='w-10 h-10 sm:hidden'
         onClick={props.displayModalHamburger}
       >
         <RxHamburgerMenu className='size-8' />
       </button>
-      <h1 className='text-4xl text-textOnLight h-12 font-semibold'>
+      <h1 className='text-4xl text-textOnLight h-10 font-semibold'>
         {props.title}
       </h1>
 
       <button
-        className={`space-x-1 h-10 sm:p-3 w-10 sm:w-auto bg-buttonDark text-textOnDark hover:bg-buttonDarkFocus flex items-center justify-center rounded-md`}
+        className={`h-10 sm:p-3 w-10 sm:w-auto bg-buttonDark text-textOnDark hover:bg-buttonDarkFocus flex items-center justify-center rounded-md`}
         onClick={props.displayModalSearch}
         aria-label='Add book'
       >
-        <IoMdAdd size='1.5em' className='text-textOnDark' />
+        <IoMdAdd className='size-8 text-textOnDark sm:hidden flex' />
         <h2 className='sm:flex hidden'>add book</h2>
       </button>
     </header>
